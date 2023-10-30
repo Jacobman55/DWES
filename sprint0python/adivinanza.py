c94bd29c1e040f8f4ec3e613694d2b20f98a9f8c
RjacoboR <92778805+Jacobman55@users.noreply.github.com>
2023-10-30 13:32:15 +0100

    Ejercicio 1: Sin nombre gracioso.

---
+++ sprint0python/adivinanza.py
@@ -0,0 +1,10 @@
+if __name__=="__main__":
+    print("Adivina adivinanza que tiene el rey en la panza")
+    print("a- Comida")
+    print("b- Ombligo")
+    print("c- manta")
+    respuesta=input("Cual es la correcta?")
+    if respuesta == "a":
+        print("Correcto")
+    else:
+        print("Error")
