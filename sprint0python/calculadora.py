125a302edce6f27b8fc79327c48413310af6d1d0
RjacoboR <92778805+Jacobman55@users.noreply.github.com>
2023-10-30 16:57:24 +0100

    Ejercicio 4 la calculadora y los int

---
+++ sprint0python/calculadora.py
@@ -0,0 +1,36 @@
+from operaciones import suma,resta,multiplicacion,division
+
+if __name__=="__main__":
+    off="no"
+    while off=="no":
+        try:
+            num1=int(input("Introduzca el primer numero"))
+            num2=int(input("Introduzca el segundo numero"))
+        except ValueError:
+            print("Solo numeros")
+        else:
+            off="si"
+
+    off=="no"
+    op=5
+    while(op<5)|(op>1):
+        print("""
+            1-Suma
+            2-Resta
+            3-Multiplicacion
+            4-División
+            5-salir""")
+        op=int(input("Que opción quieres?"))
+        while (op!=1)&(op!=2)&(op!=3)&(op!=4)&(op!=5):
+            print("Eso no está dentro de las opciones")
+            respuesta=input("Que opción quieres?")
+        if op == 1:
+            suma(num1, num2)
+        elif op == 2:
+            resta(num1, num2)
+        elif op == 3:
+            multiplicacion(num1, num2)
+        elif op == 4:
+            division(num1, num2)
+        elif op == 5:
+            off="si"
