2f9b40357d6871f83864bb71ef67e3e4bfbd44a5
jacobo <jrodriguezri@fpcoruna.afundacion.org>
2023-11-07 14:34:42 +0100

    Ejercicio 2 Sin debug y comentado

--- sprint3sesiones/www/mysite/register.php
+++ sprint3sesiones/www/mysite/register.php
@@ -4,17 +4,19 @@
 <html>
     <body>
         <?php
+        // Cojemos las variables de register.html
             $usuarios=$_POST['usuario'];
             $apellido=$_POST['apellido'];
             $email=$_POST['email'];
             $pass1=$_POST['password1'];
             $pass2=$_POST['password2'];
+            //Verificamos que la contraseña coincide
             if($pass2!=$pass1){
                 die('Las contraseñas no coinciden');
             }
-            echo '<h1> primer echo </h1>';
+            //Le hacemos hash a la contraseña para que sea segura
             $password=password_hash($pass1, PASSWORD_DEFAULT);
-            echo '<h1> segundo echo </h1>';
+            //query mas while para verificar que el correo no esta repetido
             $emailq='SELECT email FROM tUsuarios';
             $resultado=mysqli_query($db,$emailq) or die('Query error');
             while($row==mysqli_fetch_array($resultado)){
@@ -22,13 +24,14 @@
                     die('El correo ya esta registrado');
                 }
             }
-            echo '<h1> tercer echo </h1>';
+            //Impedimos una inyeccion de sql
             $comprobacion=$db->prepare("INSERT INTO tUsuarios(nombre,apellidos,email,contraseña) VALUES (?,?,?,?)");
             $comprobacion->bind_param("ssss",$usuarios,$apellido,$email,$password);
             $comprobacion->execute();
             echo '<h1>El usuario ha quedado registrado</h1>';
             echo '<a href="/register.html">Volver</a>';
             $comprobacion->close();
+            //FIN
             mysqli_close($db);
         ?>
     </body>
