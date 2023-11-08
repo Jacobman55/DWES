a56d674fcc1473b680f2ac431f84685d186a8353
jacobo <jrodriguezri@fpcoruna.afundacion.org>
2023-11-08 14:08:52 +0100

    Ejercicio 3 No se escribir en ningun idioma

---
+++ sprint3sesiones/www/mysite/login.php
@@ -0,0 +1,22 @@
+<?php
+	$db = mysqli_connect('localhost','root','1234','mysitedb') or die('Fail');
+    $email = $_POST['email'];
+    $password = $_POST['password1'];
+    $comprobacionmail=$db->prepare("SELECT id, contraseña FROM tUsuarios WHERE email =?");
+    $comprobacionmail->bind_param("s",$email);
+    $comprobacionmail->execute();
+    $resultado=$comprobacionmail->get_result();
+    if (mysqli_num_rows($resultado) > 0) {
+        $only_row = mysqli_fetch_array($resultado);
+        $verificado=password_verify($password, $only_row[1]);
+        if ($verificado){
+            session_start();
+            $_SESSION['user_id'] = $only_row[0];
+            header('Location: main.php');
+        } else {
+            echo '<p>Contraseña incorrecta</p>';
+        }
+    } else {
+        echo '<p>Usuario no encontrado con ese email</p>';
+    }
+?>
